e0985e4139341ff91884b6d943e742a5ae2799d6
DemetrioPadre <demetriopadre@gmail.com>
2024-04-04 20:49:29 +0200

    add pagination

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,10 +1,40 @@
 @extends('layouts.app')
+@section('title', 'Lista Progetti')
 
 @section('content')
     <section>
-        <div class="container">
-            <h1>Lista progetti</h1>
-            @dump($projects)
+        <div class="container ">
+            <h1 class="my-4">Lista progetti</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>TItolo</th>
+                        <th>Slug </th>
+                        <th>Estratto</th>
+                        <th></th>
+                </thead>
+                <tbody>
+                    @forelse ($projects as $project)
+                        <tr>
+                            <td>{{ $project['title'] }}</td>
+                            <td>{{ $project['slug'] }} </td>
+                            <td>{{ $project->getAbstract(50) }}</td>
+                            <td>
+                                ...actions
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="100%">
+                                <i>Nessun Risultato</i>
+                            </td>
+                        </tr>
+                    @endforelse
+                </tbody>
+
+            </table>
+            {{ $projects->links('pagination::bootstrap-5') }}
+
         </div>
     </section>
 @endsection
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
-            <a class="navbar-brand" href="#">Laravel project</a>
+            <a class="navbar-brand" href="#">I progetti di Demetrio</a>
             <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
                 class="navbar-toggler" data-bs-target="#navbarSupportedContent" data-bs-toggle="collapse" type="button">
                 <span class="navbar-toggler-icon"></span>
@@ -11,7 +11,8 @@
                     <li class="nav-item">
                         <a @class(['nav-link', 'active' => Route::currentRouteName() == 'home']) aria-current="page" href="{{ route('home') }}">Home</a>
                     </li>
-
+                </ul>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     @guest
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">Login</a>
@@ -43,7 +44,9 @@
                                 </form>
                             </div>
                         </li>
+
                     @endguest
+                </ul>
             </div>
         </div>
     </nav>
