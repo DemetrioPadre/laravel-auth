24b25a638a3a42990e89090d2386835bff1e4a7b
DemetrioPadre <demetriopadre@gmail.com>
2024-04-04 21:29:46 +0200

    add routes: show, edit,destroy

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -4,8 +4,12 @@
 @section('content')
     <section>
         <div class="container ">
-            <h1 class="my-4">Lista progetti</h1>
-            <table class="table">
+
+            <a href="{{ route('admin.project.create') }}" class="my-4 btn btn-primary"><i class="fa-solid fa-plus me-2"></i>
+                Crea un Nuovo
+                Progetto</a>
+            <h1 class="mb-4">Lista progetti</h1>
+            <table class="table mb-5">
                 <thead>
                     <tr>
                         <th>TItolo</th>
@@ -20,7 +24,19 @@
                             <td>{{ $project['slug'] }} </td>
                             <td>{{ $project->getAbstract(50) }}</td>
                             <td>
-                                ...actions
+                                <a href="{{ route('admin.project.show', $project) }}" class="btn btn-primary"><i
+                                        class="fa-solid fa-eye"></i>
+                                </a>
+                            </td>
+                            <td>
+                                <a href="{{ route('admin.project.edit', $project) }}" class="btn btn-primary"><i
+                                        class="fa-solid fa-pencil"></i>
+                                </a>
+                            </td>
+                            <td>
+                                <a href="{{ route('admin.project.destroy', $project) }}" class="btn btn-danger"><i
+                                        class="fa-solid fa-trash "></i>
+                                </a>
                             </td>
                         </tr>
                     @empty
@@ -37,4 +53,11 @@
 
         </div>
     </section>
+@endsection
+
+
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endsection
